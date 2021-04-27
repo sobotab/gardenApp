a9868c7129900c07f61c3f8e83924e4ebef54e8d
zgreenholt <zgrnhlt@udel.edu>
2021-04-27 12:15:19 -0400

    Made sure full description appears on popup screen, although image centering is a little off now

--- src/main/java/pkgView/InfoPopupView.java
+++ src/main/java/pkgView/InfoPopupView.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 import javafx.stage.Popup;
 
 public class InfoPopupView extends BorderPane{
@@ -15,15 +16,20 @@ public class InfoPopupView extends BorderPane{
 		this.setTop(title);
 		this.setAlignment(getTop(), Pos.CENTER);
 		ImageView img_copy = new ImageView(img.getImage());
-		this.setCenter(img_copy);
+		//this.setCenter(img_copy);
 		Label leps = new Label("Supports " + numLeps + " lep species.");
 		Label price = new Label("Costs " + dollars + " dollars.");
 		Label info = new Label(description);
-		HBox box = new HBox();
-		box.setSpacing(10);
-		box.getChildren().addAll(leps,info,price);
-		box.setAlignment(Pos.CENTER);
-		this.setBottom(box);
+		info.setWrapText(true);
+		info.setAlignment(Pos.CENTER);
+//		HBox box = new HBox();
+//		box.setSpacing(10);
+//		box.getChildren().addAll(leps,img_copy,price);
+//		box.setAlignment(Pos.CENTER);
+		this.setRight(price);
+		this.setLeft(leps);
+		this.setCenter(img_copy);
+		this.setBottom(info);
 		this.setPadding(new Insets(10));
 		}
 	

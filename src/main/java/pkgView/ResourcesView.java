131d2f0176f1184a7c4972460b34b2f97aee1b60
rakgad23 <rakgad@udel.edu>
2021-05-04 07:40:11 -0400

    Added Resources + Touched up on Welcome page

--- src/main/java/pkgView/ResourcesView.java
+++ src/main/java/pkgView/ResourcesView.java
@@ -9,8 +9,10 @@ import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.TilePane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import pkgController.ResourcesController;
 
 public class ResourcesView extends BorderPane{
@@ -21,10 +23,17 @@ public class ResourcesView extends BorderPane{
 		Label title = new Label("Resources");
 		title.setTextFill(Color.WHITE);
 		title.setFont(Font.font("Cambria", 80));
-		
 		Button back = new Button("Back");
 		back.setOnAction(rc.getHandlerForBack());
+		Label resource = new Label("Have you ever stopped to wonder how insects are valuable to the environment? "
+				+ "\nAlthough many may be led to believe that insects provide no ecological benefit, this is far from the case. Insects create the biological foundation for all terrestrial ecosystems. "
+				+ "\nThey cycle nutrients, pollinate plants, disperse seeds and maintain soil structure/fertility. It is critical to human society that ecosystems are properly functioning, and play an important role. "
+				+ "\nHowever, not all plants are able to support leps. Nonnative plants cannot support leps in the way that native plants can. Therefore, it is important to keep track of the native plants that are able to "
+				+ "\nsupport multiple lep species so that the environment can sustain itself. ");
+		resource.setFont(Font.font("Cambria", 14));
 		
+		//this.setCenter(resource);
+		this.setLeft(resource);
 		this.setBottom(back);
 		this.setTop(title);
 		

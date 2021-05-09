3f40bcb8bf62479a68db6cfc1e12003da96ea163
rdean-udel <rdean@udel.edu>
2021-05-09 10:04:10 -0400

    fixed an issue where plants only recognized one soil type

--- src/main/java/pkgModel/PlantGardenModel.java
+++ src/main/java/pkgModel/PlantGardenModel.java
@@ -88,24 +88,26 @@ public class PlantGardenModel extends GardenModel implements Serializable {
 		}
 		PlantObjectModel plantCheck = plants.get(index);
 		//Added this bit to account for planCheck's soil type now being string
-		Soil soil = null;
+		ArrayList<Soil> soil = new ArrayList<Soil>();
 		if(plantCheck.getSoil().contains("clay")) {
-			soil = Soil.CLAY;
+			soil.add(Soil.CLAY);
 		}
-		else if(plantCheck.getSoil().contains("sandy")) {
-			soil = Soil.SANDY;
+		if(plantCheck.getSoil().contains("sandy")) {
+			soil.add(Soil.SANDY);
 		}
-		else {
-			soil = Soil.LOAMY;
+		if(plantCheck.getSoil().contains("loamy")) {
+			soil.add(Soil.LOAMY);
 		}
 		//Added bit ends
-		Iterator validPlotsIter = plots.get( soil ).iterator();
-		
-		while (validPlotsIter.hasNext()) {		
-			Point2D.Double testPoint = new Point2D.Double(plantCheck.x - canvas_x, plantCheck.y - canvas_y);
+		for (Soil soil_type : soil) {
+			Iterator validPlotsIter = plots.get( soil_type ).iterator();
 			
-			if (inPolygon(testPoint, (ArrayList<Point2D.Double>)validPlotsIter.next())) {
-				return true;
+			while (validPlotsIter.hasNext()) {		
+				Point2D.Double testPoint = new Point2D.Double(plantCheck.x - canvas_x, plantCheck.y - canvas_y);
+				
+				if (inPolygon(testPoint, (ArrayList<Point2D.Double>)validPlotsIter.next())) {
+					return true;
+				}
 			}
 		}
 		return false;
